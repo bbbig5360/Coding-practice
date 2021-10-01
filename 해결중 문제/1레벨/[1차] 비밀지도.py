615af3ca655d56541589cc441afa8ee7b05eebc2
bbbig5360 <80315880+bbbig5360@users.noreply.github.com>
2021-10-01 18:11:46 +0900

    practice

--- 해결중 문제/1레벨/[1차] 비밀지도.py
+++ 해결중 문제/1레벨/[1차] 비밀지도.py
@@ -1,3 +1,41 @@
+# def solution(n, arr1, arr2):
+#     answer = []
+#     modular_arr1 = []
+#     modular_arr2 = []
+#     for i in range(n):
+#         modular_arr1.append([])
+#         modular_arr2.append([])
+
+#         while arr1[i] > 0:
+#             modular_arr1[i].append( arr1[i] % 2 )
+#             arr1[i] = arr1[i] // 2
+#         modular_arr1[i].reverse()
+#         while len( modular_arr1[i] ) != n:
+#             modular_arr1[i].insert(0,0)
+        
+#         while arr2[i] > 0:
+#             modular_arr2[i].append( arr2[i] % 2 )
+#             arr2[i] = arr2[i] // 2
+#         modular_arr2[i].reverse()
+#         while len( modular_arr2[i] ) != n:
+#             modular_arr2[i].insert(0,0)
+
+#     arr_len = len( modular_arr1 )
+
+#     for j in range( arr_len ) :
+#         answer.append([])
+#         for i in range(n):
+#             answer[j].append( modular_arr1[j][i] + modular_arr2[j][i] )
+        
+#     for j in range( arr_len ):
+#         for i in range(n):
+#             if answer[j][i] >= 1:
+#                 answer[j][i] = '#'
+#             else:
+#                 answer[j][i] = ' '
+#         answer[j] = ''.join(answer[j])
+#     return answer
+
 def solution(n, arr1, arr2):
     answer = []
     modular_arr1 = []
@@ -6,42 +44,23 @@ def solution(n, arr1, arr2):
         modular_arr1.append([])
         modular_arr2.append([])
 
-        # while arr1[i] > 0:
-        #     modular_arr1[i].append( arr1[i] % 2 )
-        #     arr1[i] = arr1[i] // 2
-        # modular_arr1[i].reverse()
         modular_arr1[i] = format( arr1[i], 'b')
         while len( modular_arr1[i] ) != n:
             modular_arr1[i] = '0' + modular_arr1[i]
         
-        # while arr2[i] > 0:
-        #     modular_arr2[i].append( arr2[i] % 2 )
-        #     arr2[i] = arr2[i] // 2
-        # modular_arr2[i].reverse()
         modular_arr2[i] = format( arr2[i], 'b')
         while len( modular_arr2[i] ) != n:
             modular_arr2[i] = '0' + modular_arr2[i]
 
-    arr_len = len( modular_arr1 )
-
-    for j in range( arr_len ) :
+    for j in range( len(modular_arr1) ) :
         answer.append([])
         for i in range(n):
             answer[j].append( str( int(modular_arr1[j][i]) | int(modular_arr2[j][i]) ) ) 
-
-    print(answer)
-    for j in range( arr_len ):
         answer[j] = ''.join(answer[j])
         answer[j] = answer[j].replace('0',' ')
         answer[j] = answer[j].replace('1','#')
-        # for i in range(n):
-        #     if answer[j][i] == '1':
-        #         answer[j][i] = '#'
-        #     else:
-        #         answer[j][i] = ' '
-        # answer[j] = ''.join(answer[j])
+            
+
     return answer
 
 print(solution( 6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10] ))
-
-# 해결은 했지만 코드를 더 깔끔하게 바꿔볼 것.
