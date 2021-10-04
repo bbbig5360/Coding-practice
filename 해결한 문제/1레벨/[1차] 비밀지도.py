6fdb9c634777c61ed881292f6cf338be02faa88f
bbbig5360 <bbbig5360@github.com>
2021-10-04 21:58:47 +0900

    practice

--- 해결한 문제/1레벨/[1차] 비밀지도.py
+++ 해결한 문제/1레벨/[1차] 비밀지도.py
@@ -62,20 +62,32 @@
             
     # return answer
 
+# def solution(n, arr1, arr2):
+#     answer = []
+#     for i in range(n):
+#         answer.append([[],[]])
+
+#         # 각각의 배열의 숫자를 2진수로 바꾼 뒤( foramt(,'b') )
+#         # 해당 숫자를 .zfill() 하여 요구하는 자리수에 맞는 숫자로 변형합니다.
+#         answer[i][0], answer[i][1] = format( arr1[i], 'b').zfill(n), format( arr2[i], 'b').zfill(n)
+
+#         # 변경된 숫자 두개를 or 연산처럼 계산하여 ' ' 또는 '#'을 이용한 리스트로 만듭니다.
+#         tmp = [' ' if a=='0' and b=='0' else '#' for a,b in zip(answer[i][0], answer[i][1]) ] 
+
+#         # 각 ' '와 '#'를 연결해 비밀지도를 만듭니다.
+#         answer[i] = ''.join(tmp)
+            
+#     return answer
+
 def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
-        answer.append([[],[]])
-
-        # 각각의 배열의 숫자를 2진수로 바꾼 뒤( foramt(,'b') )
-        # 해당 숫자를 .zfill() 하여 요구하는 자리수에 맞는 숫자로 변형합니다.
-        answer[i][0], answer[i][1] = format( arr1[i], 'b').zfill(n), format( arr2[i], 'b').zfill(n)
-
-        # 변경된 숫자 두개를 or 연산처럼 계산하여 ' ' 또는 '#'을 이용한 리스트로 만듭니다.
-        tmp = [' ' if a=='0' and b=='0' else '#' for a,b in zip(answer[i][0], answer[i][1]) ] 
-
-        # 각 ' '와 '#'를 연결해 비밀지도를 만듭니다.
-        answer[i] = ''.join(tmp)
+        # 두 숫자를 or 연산한 후, 2진수로 변경합니다.
+        # 후에 zfill()함수를 이용해 요구하는 자리수에 맞춥니다.
+        answer.append( format( (arr1[i] | arr2[i]), 'b').zfill(n) )
+        
+        # 나온 숫자가 0이라면 ' '를, 1이라면 '#'으로 변경해 리스트로 만듭니다.
+        answer[i] = answer[i].replace('0',' ').replace('1','#')
             
     return answer
 
