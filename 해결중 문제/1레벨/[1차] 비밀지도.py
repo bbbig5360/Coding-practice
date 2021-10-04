1171faa734fffe699e25e37cd7e550ef24c7e17a
bbbig5360 <bbbig5360@github.com>
2021-10-04 21:44:18 +0900

    practice

--- 해결중 문제/1레벨/[1차] 비밀지도.py
+++ 해결중 문제/1레벨/[1차] 비밀지도.py
@@ -36,31 +36,48 @@
 #         answer[j] = ''.join(answer[j])
 #     return answer
 
+# def solution(n, arr1, arr2):
+#     answer = []
+#     modular_arr1 = []
+#     modular_arr2 = []
+#     for i in range(n):
+#         modular_arr1.append([])
+#         modular_arr2.append([])
+
+#         modular_arr1[i] = format( arr1[i], 'b')
+#         while len( modular_arr1[i] ) != n:
+#             modular_arr1[i] = '0' + modular_arr1[i]
+        
+#         modular_arr2[i] = format( arr2[i], 'b')
+#         while len( modular_arr2[i] ) != n:
+#             modular_arr2[i] = '0' + modular_arr2[i]
+
+#     for j in range( len(modular_arr1) ) :
+#         answer.append([])
+#         for i in range(n):
+#             answer[j].append( str( int(modular_arr1[j][i]) | int(modular_arr2[j][i]) ) ) 
+#         answer[j] = ''.join(answer[j])
+#         answer[j] = answer[j].replace('0',' ')
+#         answer[j] = answer[j].replace('1','#')
+            
+    # return answer
+
 def solution(n, arr1, arr2):
     answer = []
-    modular_arr1 = []
-    modular_arr2 = []
     for i in range(n):
-        modular_arr1.append([])
-        modular_arr2.append([])
+        answer.append([[],[]])
 
-        modular_arr1[i] = format( arr1[i], 'b')
-        while len( modular_arr1[i] ) != n:
-            modular_arr1[i] = '0' + modular_arr1[i]
-        
-        modular_arr2[i] = format( arr2[i], 'b')
-        while len( modular_arr2[i] ) != n:
-            modular_arr2[i] = '0' + modular_arr2[i]
+        # 각각의 배열의 숫자를 2진수로 바꾼 뒤( foramt(,'b') )
+        # 해당 숫자를 .zfill() 하여 요구하는 자리수에 맞는 숫자로 변형합니다.
+        answer[i][0], answer[i][1] = format( arr1[i], 'b').zfill(n), format( arr2[i], 'b').zfill(n)
 
-    for j in range( len(modular_arr1) ) :
-        answer.append([])
-        for i in range(n):
-            answer[j].append( str( int(modular_arr1[j][i]) | int(modular_arr2[j][i]) ) ) 
-        answer[j] = ''.join(answer[j])
-        answer[j] = answer[j].replace('0',' ')
-        answer[j] = answer[j].replace('1','#')
+        # 변경된 숫자 두개를 or 연산처럼 계산하여 ' ' 또는 '#'을 이용한 리스트로 만듭니다.
+        tmp = [' ' if a=='0' and b=='0' else '#' for a,b in zip(answer[i][0], answer[i][1]) ] 
+
+        # 각 ' '와 '#'를 연결해 비밀지도를 만듭니다.
+        answer[i] = ''.join(tmp)
             
-
     return answer
 
-print(solution( 6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10] ))
+print( solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]) )
+print( solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10]) )
