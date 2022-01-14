d8269789ffbb4911cfa9ab785299faf64b1fe413
e2onLCM <80315880+bbbig5360@users.noreply.github.com>
2022-01-14 12:55:59 +0900

    update

--- 해결한 문제/1레벨/Roman to Integer.py
+++ 해결한 문제/1레벨/Roman to Integer.py
@@ -1,12 +1,7 @@
 ''' leetcode 문제 '''
 class Solution:
-    def __init__(self):
-        self.comb_num = {"IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
-        self.one_num = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
-        self.sum_num = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000, "IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
-        self.ret = 0
 
-    # 문자열에서 key값과 일치하는 값 * 개수 하여 결과값을 추출합니다.
+    # 후에 계산된 문자들을 삭제합니다.
     def change_num(self, input_dict, s):
         for k in input_dict.keys():
             if k in s:
@@ -15,15 +10,25 @@ class Solution:
         return s
 
     def romanToInt(self, s: str) -> int:
+        self.comb_num = {"IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
+        self.one_num = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        self.sum_num = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000, "IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
         self.ret = 0
+
+        self.ret = 0
+        # 문자열에 해당하는 값들을 추출합니다.
+        # 2개가 합쳐진 문자부터 확인합니다.
+        
+        if len(s) == 1:
+            return self.one_num[s]
         s = self.change_num(self.comb_num, s)
         if s:      
             self.change_num(self.one_num, s)
 
-        return self.ret
+        return int(self.ret)
 
     # 2개 단위로 넘어가며 합쳐질 수 있는 단어를 찾습니다.( 앞, 뒤로 )
-    def inOrder(self, s: str) -> int:
+    def inOrder(self, s):
         self.ret = 0
         total_len = len(s)
         if total_len == 1:
@@ -31,13 +36,12 @@ class Solution:
         for i in range(total_len)[::2]:
             try:
                 try:
-                    if self.sum_num[ s[i-1:i+1] ]:
-                        self.ret -= self.sum_num[s[i-1]]
-                        self.ret += self.sum_num[s[i-1:i+1]]
-                        try:
-                            self.ret += self.sum_num[s[i+1]]
-                        except:
-                            pass
+                    self.ret += self.sum_num[s[i-1:i+1]]
+                    self.ret -= self.sum_num[s[i-1]]
+                    try:
+                        self.ret += self.sum_num[s[i+1]]
+                    except:
+                        pass
                 except:
                     self.ret += self.sum_num[ s[i:i+2] ]
             except:
@@ -50,7 +54,8 @@ class Solution:
 
     # 하나씩 넘어가며 뒤에 있는 수와 합쳐질 수 있는지 확인합니다.
     # 합쳐진다면 cnt += 1 을 하여 계산한 문자는 넘어가도록 설정합니다.
-    def inOrder_upgrade(self, s: str) -> int:
+    def inOrder_upgrade(self, s):
+        self.ret = 0
         total_len = len(s)
         if total_len == 1:
             return self.sum_num[s]
@@ -70,8 +75,7 @@ class Solution:
         return self.ret
 
 roman_int = Solution()
+
 print( roman_int.inOrder("MCMXCIV") )
 print( roman_int.romanToInt("MCMXCIV") )
-# print( roman_int.romanToInt("D") )
-print( roman_int.inOrder("D") )
 print( roman_int.inOrder_upgrade('MCMXCIV'))
