f46027f37d1551855a0559fd70599c6cdbbd2776
bbbig5360 <dlckdahr777@naver.com>
2022-01-25 10:53:57 +0900

    update

--- 해결한 문제/leetcode/Reverse String.py
+++ 해결한 문제/leetcode/Reverse String.py
@@ -1,3 +1,14 @@
+''' 
+짧은 리스트에서 빠른 순서 : 1 -> 2 -> 3
+  긴 리스트에서 빠른 순서 : 1 -> 2 -> 3
+
+reverseString1 : 리스트 내장함수 reverse()사용
+reverseString2 : 슬라이싱 사용 [::-1]
+reverseString3 : reversed() 사용
+
+리스트의 길이가 짧다면 슬라이싱도 나쁘지않지만, 리스트의 크기가 커질수록 내장함수의 성능이 압도적으로 좋음
+'''
+
 class Solution:
     def reverseString1(self, s):
         s.reverse()
@@ -7,9 +18,6 @@ class Solution:
     def reverseString3(self, s):
         return list(reversed(s))
         
-
-# print( Solution().reverseString1(cmp_list) )
-# print(list(reversed(cmp_list)))
 
 from time import time
 cmp_list = list('hello')
