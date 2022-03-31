8285309db03cbc23179766eae4803811737d2167
bbbig5360 <dlckdahr777@naver.com>
2022-03-31 22:06:27 +0900

    argparse test

--- argparse_tutorial.py
+++ argparse_tutorial.py
@@ -1,17 +1,18 @@
 import argparse
+import os
 
 # 인자값을 받을 수 있는 인스턴스 생성
 parser = argparse.ArgumentParser(description='Argparse Tutorial')
 
 # 입력받을 인자값 설정 (default 값 설정가능)
-parser.add_argument('--epoch',          type=int,   default=150)
-parser.add_argument('--batch_size',     type=int,   default=128)
-parser.add_argument('--lr_initial',     type=float, default=0.1)
+parser.add_argument('--save_dir', type=str, default='save_dir')
+parser.add_argument('--time', type=int, default=30)
 
 # args 에 위의 내용 저장
 args    = parser.parse_args()
 
+os.mkdir(args.save_dir)
+
 # 입력받은 인자값 출력
-print(args.epoch)
-print(args.batch_size)
-print(args.lr_initial)
+print(args.save_dir)
+print(args.time)
