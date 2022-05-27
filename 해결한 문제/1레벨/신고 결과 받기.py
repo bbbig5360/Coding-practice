843bbfba07a21523f0bfeea9f95aa65b7beefef1
bbbig5360 <80315880+bbbig5360@users.noreply.github.com>
2022-05-27 15:20:58 +0900

    practice

---
+++ 해결한 문제/1레벨/신고 결과 받기.py
@@ -0,0 +1,49 @@
+def solution(id_list, report, k):
+    
+    if report == []:
+        return [0 for x in range(len(id_list))]
+
+    report_dict = {}
+    for id in id_list:
+        report_dict[id] = []
+
+    reported_dict = {}
+    for one_report in report:
+        report_p, reported_p = one_report.split(' ')
+        try:
+            reported_dict[reported_p] += 1
+        except:
+            reported_dict[reported_p] = 1
+
+        if reported_p not in report_dict[report_p]:
+            report_dict[report_p].append(reported_p)
+        else:
+            reported_dict[reported_p] -= 1
+
+    # print(report_dict)
+    # print(reported_dict)
+
+    punished_people = []
+
+    for key, val in reported_dict.items():
+        if val >= k:
+            punished_people.append(key)
+
+    answer = [0 for x in range(len(id_list))]
+    
+    for idx, (key, vals) in enumerate(report_dict.items()):
+        # print('key = ', key)
+        if len(vals) >= 1:
+            for val in vals:
+                if val in punished_people:
+                    print('punished +1')
+                    answer[idx] += 1
+        else:
+            continue            
+
+    return answer
+
+
+# print( solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2) )
+print('solution = ',solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
+print('sol2 = ', solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
