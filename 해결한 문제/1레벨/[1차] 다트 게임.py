25e81f678114d629059c0859ec55a5b9a3c1a544
bbbig5360 <dlckdahr777@naver.com>
2022-06-24 23:55:29 +0900

    *가 연속해서 나오는 경우 해결

--- 해결한 문제/1레벨/[1차] 다트 게임.py
+++ 해결한 문제/1레벨/[1차] 다트 게임.py
@@ -1,54 +1,60 @@
 import copy
 
 def bonus_step(ch, score):
-    # print('bonus_step')
+    score = int(score)
+    print('bonus_step', end=' ')
     if ch == 'S':
         return score
     elif ch == 'D':
-        return score * score
+        return score ** 2
     else:
-        return score * score * score
+        return score ** 3
     
 def option_step(ch, score, pre_score):
-    # print('option step')
-    # print('pre_score = ', pre_score)
-    return score * 2 + pre_score if ch == '*' else score * -1
+    print('option step')
+    print('!! pre_score = ', pre_score)
+    return (score * 2 + pre_score if ch == '*' else score * -1), True if ch in '*' else False
 
 def solution(dartResult):
     answer = 0
-    step_flag = True
     tmp_score = 0
-    special_ch = ['*', '#']
+    step_flag = True
+    star_flag = False
     
     for ch in dartResult:
-        # print(ch, end=' ')
-        # 시작이 숫자일 경우
+        print(ch, end=' ')
+        # 시작이 숫자일 경우와
         # *, # 존재할 경우 처리
         if step_flag:
             if not ch.isdigit():
-                tmp_score = option_step(ch, tmp_score, pre_score)
+                tmp_score, star_flag = option_step(ch, tmp_score, pre_score)
                 continue
             answer += tmp_score
-            pre_score = copy.deepcopy(tmp_score)
-            tmp_score = int(ch)
+            pre_score = copy.deepcopy(tmp_score) - pre_score if star_flag else copy.deepcopy(tmp_score)
+            tmp_score = ch
             step_flag = False
-        # 이어진 숫자가 존재하는 경우(10)
+            
+        # 이어진 숫자가 존재하는 경우(10)와
         # S, D, T 존재할 경우 처리
         else:
             if ch.isdigit():
-                tmp_score = int(str(tmp_score)+ch)
+                tmp_score = tmp_score + ch
                 continue
             else:
                 tmp_score = bonus_step(ch, tmp_score)
                 step_flag = True
                 
+        print('answer = ', answer)
+        
     answer += tmp_score    
     return answer
 
-print('result = ',solution('1S2D*3T')) # 37
-print('result = ',solution('1D2S#10S')) # 9
-print('result = ',solution('1D2S0T')) # 3
-print('result = ',solution('1S*2T*3S')) # 23
-print('result = ',solution('1D#2S*3S')) # 5
-print('result = ',solution('1T2D3D#')) # -4
-print('result = ',solution('1D2S3T*')) # 59
+# print('result = ',solution('1S2D*3T')) # 37
+# print('result = ',solution('1D2S#10S')) # 9
+# print('result = ',solution('1D2S0T')) # 3
+# print('result = ',solution('1S*2T*3S')) # 23
+# print('result = ',solution('1D#2S*3S')) # 5
+# print('result = ',solution('1T2D3D#')) # -4
+# print('result = ',solution('1D2S3T*')) # 59
+# print('\n1S2D*3T* result = ',solution('1S2D*3T*')) # 72
+print('\n1S*2D*3T* result = ',solution('1S*2D*3T*')) # 74
