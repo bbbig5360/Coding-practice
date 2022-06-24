bf2d5572a761ee46176c33f309d9bece9634f8c0
bbbig5360 <dlckdahr777@naver.com>
2022-06-24 20:45:22 +0900

    중복 제거

--- 해결한 문제/1레벨/[1차] 다트 게임.py
+++ 해결한 문제/1레벨/[1차] 다트 게임.py
@@ -1,11 +1,7 @@
 import copy
 
-def first_step(ch):
-    print('1st_step')
-    return int(ch)
-
-def second_step(ch, score):
-    print('2nd_step')
+def bonus_step(ch, score):
+    # print('bonus_step')
     if ch == 'S':
         return score
     elif ch == 'D':
@@ -13,56 +9,46 @@ def second_step(ch, score):
     else:
         return score * score * score
     
-def third_step(ch, score, pre_score):
-    print('3rd step')
-    print('pre_score = ', pre_score)
+def option_step(ch, score, pre_score):
+    # print('option step')
+    # print('pre_score = ', pre_score)
     return score * 2 + pre_score if ch == '*' else score * -1
 
 def solution(dartResult):
     answer = 0
-    step_flag = 0
+    step_flag = True
     tmp_score = 0
     special_ch = ['*', '#']
     
     for ch in dartResult:
-        print(ch, end=' ')
-        # 1단계. 시작 숫자 확보.
-        # *#이 연속으로 들어갔을 경우 처리.
-        if step_flag == 0:
+        # print(ch, end=' ')
+        # 시작이 숫자일 경우
+        # *, # 존재할 경우 처리
+        if step_flag:
             if not ch.isdigit():
-                tmp_score = third_step(ch, tmp_score, pre_score)
+                tmp_score = option_step(ch, tmp_score, pre_score)
                 continue
             answer += tmp_score
             pre_score = copy.deepcopy(tmp_score)
-            tmp_score = first_step(ch)
-            
-        # S, D, T 처리
-        elif step_flag == 1:
-            if ch.isdigit():
-                tmp_score = first_step(str(tmp_score)+ch)
-                step_flag = 0
-            else:
-                tmp_score = second_step(ch, tmp_score)
-            
-        # 입력된 형식이 끝나거나 *,#이 나왔을 경우 처리
+            tmp_score = int(ch)
+            step_flag = False
+        # 이어진 숫자가 존재하는 경우(10)
+        # S, D, T 존재할 경우 처리
         else:
-            if ch in special_ch:
-                tmp_score = third_step(ch, tmp_score, pre_score)
-                step_flag = -1
+            if ch.isdigit():
+                tmp_score = int(str(tmp_score)+ch)
+                continue
             else:
-                answer += tmp_score
-                pre_score = copy.deepcopy(tmp_score)
-                tmp_score = first_step(ch)
-                step_flag = 0                
-        step_flag += 1
-        
+                tmp_score = bonus_step(ch, tmp_score)
+                step_flag = True
+                
     answer += tmp_score    
     return answer
 
-# print('result = ',solution('1S2D*3T')) # 37
-# print('result = ',solution('1D2S#10S')) # 9
-# print('result = ',solution('1D2S0T')) # 3
-# print('result = ',solution('1S*2T*3S')) # 23
-# print('result = ',solution('1D#2S*3S')) # 5
-# print('result = ',solution('1T2D3D#')) # -4
+print('result = ',solution('1S2D*3T')) # 37
+print('result = ',solution('1D2S#10S')) # 9
+print('result = ',solution('1D2S0T')) # 3
+print('result = ',solution('1S*2T*3S')) # 23
+print('result = ',solution('1D#2S*3S')) # 5
+print('result = ',solution('1T2D3D#')) # -4
 print('result = ',solution('1D2S3T*')) # 59
