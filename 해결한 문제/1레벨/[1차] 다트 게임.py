25a3d85129109d79ba44bb0fe12bd45567404fd6
bbbig5360 <dlckdahr777@naver.com>
2022-06-27 00:20:30 +0900

    추가 테스트 케이스에서 오류 못 찾음 -> 간단한 v2 만들어서 실행 예정

--- 해결한 문제/1레벨/[1차] 다트 게임.py
+++ 해결한 문제/1레벨/[1차] 다트 게임.py
@@ -1,8 +1,9 @@
 import copy
 
 def bonus_step(ch, score):
+    # print('bonus_step')
     score = int(score)
-    print('bonus_step', end=' ')
+    
     if ch == 'S':
         return score
     elif ch == 'D':
@@ -11,9 +12,8 @@ def bonus_step(ch, score):
         return score ** 3
     
 def option_step(ch, score, pre_score):
-    print('option step')
-    print('!! pre_score = ', pre_score)
-    return (score * 2 + pre_score if ch == '*' else score * -1), True if ch in '*' else False
+    # print('option step, pre_score = ', pre_score)
+    return (2*score + pre_score if ch == '*' else -score), True if ch in '*' else False
 
 def solution(dartResult):
     answer = 0
@@ -22,14 +22,17 @@ def solution(dartResult):
     star_flag = False
     
     for ch in dartResult:
-        print(ch, end=' ')
+        # print(ch, end=' ')
         # 시작이 숫자일 경우와
-        # *, # 존재할 경우 처리
+        # *, # 존재할 경우
+        # 마지막 결과 총합까지 처리
         if step_flag:
             if not ch.isdigit():
                 tmp_score, star_flag = option_step(ch, tmp_score, pre_score)
                 continue
             answer += tmp_score
+            # print('answer = ', answer)
+            
             pre_score = copy.deepcopy(tmp_score) - pre_score if star_flag else copy.deepcopy(tmp_score)
             tmp_score = ch
             step_flag = False
@@ -43,18 +46,26 @@ def solution(dartResult):
             else:
                 tmp_score = bonus_step(ch, tmp_score)
                 step_flag = True
-                
-        print('answer = ', answer)
         
     answer += tmp_score    
     return answer
 
-# print('result = ',solution('1S2D*3T')) # 37
-# print('result = ',solution('1D2S#10S')) # 9
-# print('result = ',solution('1D2S0T')) # 3
-# print('result = ',solution('1S*2T*3S')) # 23
-# print('result = ',solution('1D#2S*3S')) # 5
-# print('result = ',solution('1T2D3D#')) # -4
-# print('result = ',solution('1D2S3T*')) # 59
-# print('\n1S2D*3T* result = ',solution('1S2D*3T*')) # 72
-print('\n1S*2D*3T* result = ',solution('1S*2D*3T*')) # 74
+print('result = ',solution('1S2D*3T')) # 37
+print('result = ',solution('1D2S#10S')) # 9
+print('result = ',solution('1D2S0T')) # 3
+print('result = ',solution('1S*2T*3S')) # 23
+print('result = ',solution('1D#2S*3S')) # 5
+print('result = ',solution('1T2D3D#')) # -4
+print('result = ',solution('1D2S3T*')) # 59
+print('result = ',solution('1S2D*3T*')) # 72
+print('result = ',solution('1S*2D*3T*')) # 74
+print('result = ',solution('1S#2D*3T*')) # -1 -1 + 8 + 8 + 54 = 68
+print('result = ',solution('1D#2S#0T*')) # -1 -2 -2 + 0 = -5
+print('result = ',solution('10S*2T*10S')) # 20 + 20 + 16 + 10 = 66
+print('result = ',solution('10S*2T#3S*')) # 20 - 8 - 8 + 6 = 10
+print('result = ',solution('10S*2T#3S*5S*2D#')) # 20 - 8 - 8 + 6 + 6 + 10 - 4 = 22
+print('result = ',solution('10S*2T#3S5S*2D#')) # 20 - 8 + 3 + 3 + 10 - 4 = 24
+print('result = ',solution('1S2T#3S5S*2D#')) # 1 - 8 + 3 + 3 + 10 - 4 = 5
+print('result = ',solution('2T#3S5S*2D#')) # -8 + 3 + 3 + 10 - 4 = 4
+print('result = ',solution('2T#3S*5S2D#')) # -8 -8 + 6 + 5 - 4 = -9
+print('result = ',solution('2T#3S*5S2D')) # -8 -8 + 6 + 5 + 4 = -1
