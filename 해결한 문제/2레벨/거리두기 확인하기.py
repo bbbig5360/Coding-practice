b010033546e801374856c2a5fb8ddc8bc8747d0f
bbbig5360 <dlckdahr777@naver.com>
2022-06-27 20:16:34 +0900

    바로 옆이 아닌 O가 나오는 경우. 즉, POP도 탐색 가능토록 변경

--- 해결한 문제/2레벨/거리두기 확인하기.py
+++ 해결한 문제/2레벨/거리두기 확인하기.py
@@ -1,37 +1,80 @@
-def right_check(places, row, row_idx, col_idx):
+def all_check(places, row_idx, col_idx, order):
+    check_position = [[0,1],[1,0],[-1,0],[0,-1]]
+    check_direction = [0, 1, 2, 3]
+    reverse_dir_dict = {0:3, 1:2, 2:1, 3:0}
+    # {'right':0, 'under':1, 'up':2, 'left':3}
     ret = False
-    try:
-        ret = True if places[row_idx][col_idx + 1] == 'P' else False
-    except:
-        pass
+    for pos, dir in zip(check_position, check_direction):
+        check_dir = reverse_dir_dict[dir]
+        # 예외. 왼쪽에서 시작했다면 왼쪽은 제외하고 상하좌우 확인
+        if order != check_dir:
+            tmp_row_idx = row_idx + pos[0]
+            tmp_col_idx = col_idx + pos[1]
+
+            if tmp_row_idx < 0 or tmp_col_idx < 0:
+                continue
+            try:
+                if places[tmp_row_idx][tmp_col_idx] == 'P':
+                    ret = True
+            except:
+                pass
+        if ret:
+            print('POP find : ', row_idx, col_idx, ' -> ', tmp_row_idx, tmp_col_idx)
+            break
     return ret
 
-def under_check(places, row, row_idx, col_idx):
+def check_element(places, row_idx, col_idx, order):
     ret = False
-    try:
-        ret = True if places[row_idx+1][col_idx] == 'P' else False
+    order_dict = {'right':0, 'under':1, 'up':2, 'left':3}
+    pos_dict = {0:[0,1], 1:[1,0], 2:[-1,0], 3:[0,-1]}
+    
+    order = order_dict[order]
+    row_idx += pos_dict[order][0]
+    col_idx += pos_dict[order][1]
+    # 방향에 맞는 위치로 이동
+        
+    try: 
+        check_ch = places[row_idx][col_idx]
+        if check_ch == 'P':
+            ret = True 
+        elif check_ch == 'O':
+            ret = all_check(places, row_idx, col_idx, order)
+        else:
+            pass
     except:
         pass
-    return ret
+    
+    return ret    
 
 def solution(places):
     answer = []
-    tmp_class = []
-    ret = False
+    
     for place in places:
+        right_ret = False
+        under_ret = False
+        ret = False
         for row_idx, row in enumerate(place):
-            print(row)
             for col_idx, ch in enumerate(row):
+                # 사람이 있는지 확인. 이후 오른쪽과 아래쪽 확인
+                # 오른쪽과 아래로 훑어가면 전부 훑게되기 때문
+                # 이후 O가 나온다면 상하좌우 체크 = all_check()함수
                 if ch == 'P':
-                    right_ret = right_check(places, row, row_idx, col_idx)
-                    under_ret = under_check(places, row, row_idx, col_idx)
-                if right_ret or under_ret:
-                    print(right_ret, under_ret)
-                    break
-            if ret:
+                    right_ret = check_element(place, row_idx, col_idx, 'right')
+                    under_ret = check_element(place, row_idx, col_idx, 'under')
+                    if right_ret or under_ret:
+                        break
+            if right_ret or under_ret:
+                ret = True
                 break
-        print()
         answer.append(0 if ret else 1)
     return answer        
 
-print('result = ', solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]], ))
+print('result = ', solution([["POOOP","OXXOX","OPXPX","OOXOX","POXXP"],
+                             ["POOPX","OXPXP","PXXXO","OXXXO","OOOPP"], 
+                             ["PXOPX","OXOXP","OXPOX","OXXOP","PXPOX"], 
+                             ["OOOXX","XOOOX","OOOXX","OXOOX","OOOOO"],
+                             ["PXPXP","XPXPX","PXPXP","XPXPX","PXPXP"]] ))
+
+# print('result = ', solution([["PXPXP",
+#                               "XXXPX",
+#                               "XXPOX"],]))
