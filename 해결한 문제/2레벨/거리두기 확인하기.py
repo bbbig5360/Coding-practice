69a2f98ff85a671ec4266a085130bf53dbb57350
bbbig5360 <dlckdahr777@naver.com>
2022-06-30 03:37:23 +0900

    예외 케이스 확인. OPXXX, PXOOO인 경우

--- 해결한 문제/2레벨/거리두기 확인하기.py
+++ 해결한 문제/2레벨/거리두기 확인하기.py
@@ -6,7 +6,8 @@ def all_check(places, row_idx, col_idx, order):
     ret = False
     for pos, dir in zip(check_position, check_direction):
         check_dir = reverse_dir_dict[dir]
-        # 예외. 왼쪽에서 시작했다면 왼쪽은 제외하고 상하좌우 확인
+        # 시작한 방향 예외처리. 
+        # 예시) 왼쪽에서 시작했다면 왼쪽은 제외하고 상하좌우 확인
         if order != check_dir:
             tmp_row_idx = row_idx + pos[0]
             tmp_col_idx = col_idx + pos[1]
@@ -16,11 +17,10 @@ def all_check(places, row_idx, col_idx, order):
             try:
                 if places[tmp_row_idx][tmp_col_idx] == 'P':
                     ret = True
+                    # print('POP find : ', row_idx, col_idx, ' -> ', tmp_row_idx, tmp_col_idx)
+                    break
             except:
                 pass
-        if ret:
-            print('POP find : ', row_idx, col_idx, ' -> ', tmp_row_idx, tmp_col_idx)
-            break
     return ret
 
 def check_element(places, row_idx, col_idx, order):
@@ -39,6 +39,17 @@ def check_element(places, row_idx, col_idx, order):
             ret = True 
         elif check_ch == 'O':
             ret = all_check(places, row_idx, col_idx, order)
+        elif order == 0 and check_ch == 'X':
+            # 예외 케이스. 오른쪽과 아래만 확인하다보니 생기는 문제. 
+            # OPXXX, PXOOO의 경우에 찾지못함.
+            try:
+                row_idx -= 1
+                col_idx -= 1
+
+                if places[row_idx][col_idx] == 'O' and places[row_idx][col_idx+1] == 'P':
+                        ret = True
+            except:
+                pass
         else:
             pass
     except:
@@ -48,6 +59,7 @@ def check_element(places, row_idx, col_idx, order):
 
 def solution(places):
     answer = []
+    ret = False
     
     for place in places:
         right_ret = False
@@ -62,19 +74,23 @@ def solution(places):
                     right_ret = check_element(place, row_idx, col_idx, 'right')
                     under_ret = check_element(place, row_idx, col_idx, 'under')
                     if right_ret or under_ret:
+                        ret = True
                         break
-            if right_ret or under_ret:
-                ret = True
+            if ret:
                 break
         answer.append(0 if ret else 1)
     return answer        
 
-print('result = ', solution([["POOOP","OXXOX","OPXPX","OOXOX","POXXP"],
-                             ["POOPX","OXPXP","PXXXO","OXXXO","OOOPP"], 
-                             ["PXOPX","OXOXP","OXPOX","OXXOP","PXPOX"], 
-                             ["OOOXX","XOOOX","OOOXX","OXOOX","OOOOO"],
-                             ["PXPXP","XPXPX","PXPXP","XPXPX","PXPXP"]] ))
+# print('result = ', solution([["POOOP","OXXOX","OPXPX","OOXOX","POXXP"],
+#                              ["POOPX","OXPXP","PXXXO","OXXXO","OOOPP"], 
+#                              ["PXOPX","OXOXP","OXPOX","OXXOP","PXPOX"], 
+#                              ["OOOXX","XOOOX","OOOXX","OXOOX","OOOOO"],
+#                              ["PXPXP","XPXPX","PXPXP","XPXPX","PXPXP"]] ))
 
-# print('result = ', solution([["PXPXP",
-#                               "XXXPX",
-#                               "XXPOX"],]))
+print('result = ', solution([["OPXOP", 
+                              "PXPXO",
+                              "OOXPO"],]))
+                              
+# 오류 케이스 찾음.                              
+# OP
+# PX
