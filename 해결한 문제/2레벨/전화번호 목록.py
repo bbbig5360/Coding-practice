733d9d6d41494679561378481de81c381b33f401
bbbig5360 <dlckdahr777@naver.com>
2022-07-11 03:02:02 +0900

    접두사 확인시 startswith 사용

--- 해결한 문제/2레벨/전화번호 목록.py
+++ 해결한 문제/2레벨/전화번호 목록.py
@@ -1,21 +1,19 @@
 def solution(phone_book):
-    answer = True
     phone_book.sort()
     tot_len = len(phone_book)
 
     for i in range(tot_len):
         if tot_len > i + 1:
-            start_num = phone_book[i]
+            x = phone_book[i]
             y = phone_book[i+1]
             
-            if start_num > y:
-                start_num, y = y, start_num
+            if x > y:
+                x, y = y, x
                 
-            if start_num in y[:len(start_num)]:
-                answer = False
-                break
+            if y.startswith(x):
+                return False
         
-    return answer
+    return True
 
 print(solution(["119", "97674223", "1195524421"]))
 print(solution(["123","456","789"]))
