ae1e8adcbf2d4c48851c1ed0f20524841bd5eb7e
bbbig5360 <dlckdahr777@naver.com>
2022-07-11 02:53:02 +0900

    sort 이후 앞, 뒤 문자 사이의 접두관계 있는지 확인으로 효율석 문제 풀이 성공

--- 해결한 문제/2레벨/전화번호 목록.py
+++ 해결한 문제/2레벨/전화번호 목록.py
@@ -1,20 +1,24 @@
 def solution(phone_book):
     answer = True
-    
-    for idx, word in enumerate(phone_book):
-        word_len = len(word)
-        
-        for idx2, ch in enumerate(phone_book):
-            if idx == idx2:
-                continue
-            if word == ch[:word_len]:
-                answer = False
-                break
+    phone_book.sort()
+    tot_len = len(phone_book)
+
+    for i in range(tot_len):
+        if tot_len > i + 1:
+            x = phone_book[i]
+            y = phone_book[i+1]
             
-        if answer == False:
-            break
+            if x < y:
+                if x in y[:len(x)]:
+                    answer = False
+                    break
+            else:
+                if y in x[:len(y)]:
+                    answer = False
+                    break
+        
     return answer
 
 print(solution(["119", "97674223", "1195524421"]))
 print(solution(["123","456","789"]))
-print(solution(["12","123","1235","567","88"]))
+print(solution(["12","1234121","1235","567","88"]))
