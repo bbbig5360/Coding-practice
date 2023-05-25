e59ce3a06819b3fab3edf74c007830ce02f01791
bbbig5360 <dlckdahr777@naver.com>
2023-05-25 15:53:12 +0900

    product repeat 사용 & 주석들 수정

--- 해결한 문제/2레벨/이모티콘 할인행사.py
+++ 해결한 문제/2레벨/이모티콘 할인행사.py
@@ -1,25 +1,22 @@
-from itertools import combinations_with_replacement, permutations, product
+from itertools import product
 
 def solution(users, emoticons):
-    answer = [0,0]
-    rate_list = [40, 30, 20, 10]     
+    rate_list = [40, 30, 20, 10]
     emot_len = len(emoticons)
-    total_rate_list = []
-    for x in range(emot_len):
-        total_rate_list.append(rate_list)
-    total_rate_list = list(product(*total_rate_list))    
-    # print('total_rate_list =',total_rate_list)
-    # print('len = ', len(total_rate_list))
-    # total_rate_list = list/(permutations(rate_list, emot_len))    
-    # print('total_rate_list =',total_rate_list)
+    total_rate_list = list(product(rate_list,repeat=emot_len))
     
+    # {(할인율1, 할인율1, 할인율1) : [user1의 멤버 가입 여부, user1의 이모티콘 구매가격],
+    #  (할인율1, 할인율1, 할인율2) : [user2의 멤버 가입 여부, user2의 이모티콘 구매가격],
+    #  (할인율1, 할인율2, 할인율1) : [user3의 멤버 가입 여부, user3의 이모티콘 구매가격],
+    #  ...
+    # }
     tmp_dict = {}
     
     # 해당 할인율일때 최대의 구독자수를 구한다.
     # 최대 구독자수에서 최대의 가격을 찾는다.
     for rate in total_rate_list:
         tmp_dict[rate] = []
-
+        
         for idx, user in enumerate(users):
             price = 0
             tmp_dict[rate].append([0,0])
@@ -28,7 +25,6 @@ def solution(users, emoticons):
             for i in range(emot_len):                
                 if  rate[i] >= user[0]:
                     price += emoticons[i] * (100-rate[i])
-                    # print(f'user  : {user} >> i, rate, emotions, price = {i} {rate[i]} {emoticons[i]} {price/100}')
             price = int(price/100)
 
             # 해당 가격일때 구독한다면 바로 끝. 아니라면 price 계산
@@ -39,7 +35,11 @@ def solution(users, emoticons):
                 if tmp_dict[rate][idx][1] < price:
                     tmp_dict[rate][idx][1] = price
     
-    # print('tmp_dict =',tmp_dict)
+
+    answer = [0,0]    
+    
+    # 각 할인율에서 최대 멤버 가입수에 따른 최대 이모티콘 구매 가격을 추출
+    print('tmp_dict =',tmp_dict)
     for k, val in tmp_dict.items():            
             tmp_ret = [0,0]    
             for v in val:
@@ -56,6 +56,5 @@ def solution(users, emoticons):
     
     return answer
 
-
 print(solution([[40,10000], [25,10000]], [7000,9000]))
-print(solution([[40, 2900], [23, 10000], [11, 5200], [5, 5900], [40, 3100], [27, 9200], [32, 6900]], [1300, 1500, 1600, 4900]))
+# print(solution([[40, 2900], [23, 10000], [11, 5200], [5, 5900], [40, 3100], [27, 9200], [32, 6900]], [1300, 1500, 1600, 4900]))
