0623bb7c8606481554a6ab7df0e18ce8670b6230
bbbig5360 <dlckdahr777@naver.com>
2023-06-07 20:42:18 +0900

    13, 14 시간 초과

--- 해결한 문제/2레벨/메뉴 리뉴얼.py
+++ 해결한 문제/2레벨/메뉴 리뉴얼.py
@@ -1,3 +1,4 @@
+from time import time
 import itertools
 
 def solution(orders, course):
@@ -8,44 +9,57 @@ def solution(orders, course):
         single_menu.add(menu)
     single_menu = sorted(single_menu)    
     
+    
+    
     for c_num in course:
-        course_combination_list = list(map(''.join,itertools.combinations(single_menu, c_num)))
-        course_dict = {}
+        if len(single_menu) < c_num:
+            continue
+        st = time()
+        course_combination_list = list(itertools.combinations(single_menu, c_num))
+        print('combination time = ', time() - st )
+
+        course_dict = {}    
         for order in orders:
+            # collections Counter 사용하는것과 같음
             for course_combination in course_combination_list:
                 flag = True
                 for c in course_combination:
-                    if c in order:
-                        continue
-                    else:
+                    if c not in order:
                         flag = False
+                        break
                 if flag:
                     if course_dict.get(course_combination) != None:
                         course_dict[course_combination] += 1
                     else:
                         course_dict[course_combination] = 1
+        print('get courst_dict time = ', time() - st)
+        st = time()
         
-        tmp = ''
-        max_cnt = 0
+        # collections의 .most_common과 같음
+        tmp = []
+        max_cnt = 2
         for combination, count  in course_dict.items():
-            if count < 2:
-                continue
             if max_cnt < count:
                 max_cnt = count
-                tmp = combination
-        if tmp != '':
-            answer.add(tmp)
+                tmp.clear()
+                tmp.append(combination)
+            elif max_cnt == count:
+                tmp.append(combination)
+                
+        for t in tmp:
+            answer.add(''.join(t))
             
-        for combination, count  in course_dict.items():
-            if max_cnt == 0:
-                break
-            if max_cnt == count:
-                answer.add(combination)
-        
         course_dict.clear()
-    
+
+        print('calcurate max count time = ',time() - st)
     return sorted(answer)
 
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
-print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
-print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
+# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+# print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
+print(solution(["ABCDEFGHI", "ABDGHIZ", "CDXJKWZ", "ADEFGIK", "LMNOXYZ", "PQXYZ", "ACDRZ"], [5,6,7]))
+# print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
+# print(solution(["XYZ", "XWY", "WXA"], [1,2,3,4]))
+
+
+
+print(list(map(''.join,itertools.combinations(['a','b'], 3))))
