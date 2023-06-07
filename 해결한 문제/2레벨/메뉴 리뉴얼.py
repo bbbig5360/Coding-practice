f95ccaf885ac7da14d132f4cf35accae4eeafa48
bbbig5360 <dlckdahr777@naver.com>
2023-06-07 21:34:33 +0900

    order의 길이가 course의 길이보다 짧다면 continue시키는 조건 설정으로 시간 초과 해결

--- 해결한 문제/2레벨/메뉴 리뉴얼.py
+++ 해결한 문제/2레벨/메뉴 리뉴얼.py
@@ -1,26 +1,23 @@
-from time import time
 import itertools
 
 def solution(orders, course):
-    answer = set()
+    answer = []
     single_menu = set()
     
     for menu in ''.join(orders):
         single_menu.add(menu)
-    single_menu = sorted(single_menu)    
-    
-    
+    single_menu = sorted(single_menu)
     
     for c_num in course:
         if len(single_menu) < c_num:
             continue
-        st = time()
+        
         course_combination_list = list(itertools.combinations(single_menu, c_num))
-        print('combination time = ', time() - st )
 
         course_dict = {}    
         for order in orders:
-            # collections Counter 사용하는것과 같음
+            if len(order) < c_num:
+                continue
             for course_combination in course_combination_list:
                 flag = True
                 for c in course_combination:
@@ -32,10 +29,7 @@ def solution(orders, course):
                         course_dict[course_combination] += 1
                     else:
                         course_dict[course_combination] = 1
-        print('get courst_dict time = ', time() - st)
-        st = time()
         
-        # collections의 .most_common과 같음
         tmp = []
         max_cnt = 2
         for combination, count  in course_dict.items():
@@ -47,19 +41,12 @@ def solution(orders, course):
                 tmp.append(combination)
                 
         for t in tmp:
-            answer.add(''.join(t))
+            answer.append(''.join(t))
             
         course_dict.clear()
 
-        print('calcurate max count time = ',time() - st)
     return sorted(answer)
 
-# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
-# print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
-print(solution(["ABCDEFGHI", "ABDGHIZ", "CDXJKWZ", "ADEFGIK", "LMNOXYZ", "PQXYZ", "ACDRZ"], [5,6,7]))
-# print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
-# print(solution(["XYZ", "XWY", "WXA"], [1,2,3,4]))
-
-
-
-print(list(map(''.join,itertools.combinations(['a','b'], 3))))
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
+print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
