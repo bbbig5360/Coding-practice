80c8d3a33dcd9e07a7809d3b294a2ab638de2069
bbbig5360 <dlckdahr777@naver.com>
2023-06-15 19:43:41 +0900

    info의 점수순으로 정렬 후 binary search로 검색시간 단축. 시간 초과 동일

--- 해결한 문제/2레벨/순위 검색.py
+++ 해결한 문제/2레벨/순위 검색.py
@@ -1,35 +1,57 @@
-from collections import defaultdict
-
+def binary_search(max, tmp_info_list, score):
+    start = 0
+    end = max
+    
+    while start != end and start != max:
+        if tmp_info_list[(start+end)//2][0] >= int(score):
+            end = (start+end)//2
+        else:
+            start = (start+end)//2 + 1
+    return start
+    
 def solution(info, query):
     answer = []
     info_list = []
+    info_len = len(info)
     
-    for info_row in info:
-        split_list = info_row.split()
-        tmp_dict = defaultdict(lambda:int(split_list[-1]))
-        for element in split_list:
-            tmp_dict[element]
-        info_list.append(tmp_dict)    
+    # st = time.time()
+    for idx, info_row in enumerate(info):
+        score, food, career, ability, language = info_row.split()[::-1]
+        info_row = [int(score), food, career, ability, language]
+        info_list.append(info_row)
+
+    info_list.sort()
+    # print(info_list)
     
     for query_row in query:
-        query_list = [ x for x in query_row.split() if x != 'and']
-    
-        cnt = 0
-        for info_row in info_list:      
-            query_idx = -1
-            for query_element in query_list:
-                if query_element == '-':
-                    query_idx += 1
-                    continue
-                query_idx += 1
+        # st = time.time()
+        query_list = [x for x in query_row.split()[::-1] if x != 'and']
+        # print('query_list =', query_list)
+        
+        info_strainer = [True]*info_len
+        target_idx = 0
+        for idx, query_element in enumerate(query_list):
+            # print('idx', idx)
+            # print(info_row[idx],query_element)
+            if query_element == '-':
+                continue
+            
+            if idx == 0:
+                # 여기서 점수값 위치 binary search
+                target_idx = binary_search(info_len, info_list, query_element)
                 
-                if query_idx != 4:
-                    if query_element not in info_row:
-                        break
-                else:
-                    if int(query_element) <= list(info_row.values())[0]:
-                        cnt += 1
-        answer.append(cnt)
+            for info_idx in range(target_idx, info_len):
+                # print(info_row[idx], query_element)
+                if info_strainer[info_idx] == False:
+                    continue
+                
+                if idx != 0:
+                    if info_list[info_idx][idx] != query_element:
+                        info_strainer[info_idx] = False
+                        continue
+                    
+        answer.append(info_strainer.count(True)-target_idx)
+        # print(query_row, (time.time() - st)*(100000))
     return answer
 
 
